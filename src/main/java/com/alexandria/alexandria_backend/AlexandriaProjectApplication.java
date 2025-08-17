69183d4bebc9dbb1f1239d800b7c514708c6eaca
gchagas09 <gchagas090299@outlook.com.br>
2025-08-17 09:48:37 -0300

    add: arquivos do spring boot
    Arquivos do SpringBoot Adicionados, bem como modificação no arquivo .gitignore

---
+++ src/main/java/com/alexandria/alexandria_backend/AlexandriaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alexandria.alexandria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandriaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandriaProjectApplication.class, args);
+	}
+
+}
